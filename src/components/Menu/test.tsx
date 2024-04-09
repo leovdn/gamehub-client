fa6aeec07e03c13947c939bcded11a1c00ca825e
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-04-09 18:25:06 -0300

    test: logged  user cases

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -33,4 +33,22 @@ describe('<Menu />', () => {
     expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
     expect(fullMenuElement).toHaveStyle({ opacity: 0 })
   })
+
+  it('should show register box when logged out', () => {
+    renderWithTheme(<Menu />)
+
+    expect(screen.queryByText(/My Account/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/Wishlist/i)).not.toBeInTheDocument()
+    expect(screen.getByText(/Log in now/i)).toBeInTheDocument()
+    expect(screen.getByText(/Sign up/i)).toBeInTheDocument()
+  })
+
+  it('should show wishlist and account', () => {
+    renderWithTheme(<Menu username="John Doe" />)
+
+    expect(screen.getByText(/My Account/i)).toBeInTheDocument()
+    expect(screen.getByText(/Wishlist/i)).toBeInTheDocument()
+    expect(screen.queryByText(/Log in now/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/Sign up/i)).not.toBeInTheDocument()
+  })
 })
