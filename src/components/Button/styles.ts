12a6cd5a470468abc30ca5659a228ac8c1b4e8eb
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-04-22 16:11:34 -0300

    style:  button link and size

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -25,10 +25,6 @@ const wrapperModifiers = {
     width: 100%;
   `,
   withIcon: (theme: DefaultTheme) => css`
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-
     svg {
       width: 1.5rem;
 
@@ -41,12 +37,16 @@ const wrapperModifiers = {
 
 export const Wrapper = styled.button<WrapperProps>`
   ${({ theme, size, fullWidth, hasIcon }) => css`
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
     padding: ${theme.spacings.xxsmall};
     border-radius: ${theme.border.radius};
 
     border: none;
     background: linear-gradient(180deg, #ff4f5f 0%, #f062c0 50%);
     color: ${theme.colors.white};
+    text-decoration: none;
     cursor: pointer;
 
     &:hover {
