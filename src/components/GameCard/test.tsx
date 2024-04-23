909a05d1bde79d2ad8bf18c7720e2be206e14ffe
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-04-23 12:51:44 -0300

    test: gamecard price behaviour

---
+++ src/components/GameCard/test.tsx
@@ -0,0 +1,73 @@
+import { screen } from '@testing-library/react'
+
+import GameCard from './index'
+import { renderWithTheme } from 'utils/tests/helpers'
+import { ClassAttributes, ImgHTMLAttributes } from 'react'
+
+const props = {
+  title: 'Population Zero',
+  developer: 'Rockstar Studios',
+  img: '/img/gamecard-img.png',
+  price: 'R$ 235,00'
+}
+
+jest.mock('next/image', () => ({
+  __esModule: true,
+  default: (
+    props: JSX.IntrinsicAttributes &
+      ClassAttributes<HTMLImageElement> &
+      ImgHTMLAttributes<HTMLImageElement>
+  ) => <img {...props} />
+}))
+
+describe('<GameCard />', () => {
+  it('should render correctly', () => {
+    renderWithTheme(<GameCard {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: props.title })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: props.developer })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      props.img
+    )
+    expect(screen.getByText(props.price)).toBeInTheDocument()
+
+    expect(screen.getByLabelText(/add to wishlist/i)).toBeInTheDocument()
+  })
+
+  it('should render price in label', () => {
+    renderWithTheme(<GameCard {...props} />)
+
+    //price doesn't have line-through
+    expect(screen.getByText(props.price)).not.toHaveStyle({
+      textDecoration: 'line-through'
+    })
+    // price doesn't have gray color
+    expect(screen.getByText(props.price)).not.toHaveStyle({
+      color: '#8F8F8F'
+    })
+    // price have secondary background
+    expect(screen.getByText(props.price)).toHaveStyle({
+      backgroundColor: '#3CD3C1'
+    })
+  })
+
+  it('should render a line-through when price is promotional', () => {
+    renderWithTheme(<GameCard promotionalPrice="R$ 200,00" {...props} />)
+
+    //price has line-through
+    expect(screen.getByText(props.price)).toHaveStyle({
+      textDecoration: 'line-through'
+    })
+    //new promotional price doesn't have line-through
+    expect(screen.getByText('R$ 200,00')).not.toHaveStyle({
+      textDecoration: 'line-through'
+    })
+  })
+})
