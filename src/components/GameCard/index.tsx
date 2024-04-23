f173480e0962c50c0e362aa273babd546d3b99d8
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-04-23 12:51:12 -0300

    feat: create gamecard

---
+++ src/components/GameCard/index.tsx
@@ -0,0 +1,47 @@
+import Image from 'next/image'
+import * as S from './styles'
+import { AddShoppingCart, FavoriteBorder } from 'styled-icons/material-outlined'
+import Button from 'components/Button'
+
+export type GameCardProps = {
+  title: string
+  developer: string
+  img: string
+  price: string
+  promotionalPrice?: string
+}
+
+const GameCard = ({
+  title,
+  developer,
+  img,
+  price,
+  promotionalPrice
+}: GameCardProps) => {
+  return (
+    <S.Wrapper>
+      <S.ImageBox>
+        <Image src={img} alt={title} width={300} height={140} />
+      </S.ImageBox>
+
+      <S.Content>
+        <S.Info>
+          <S.Title>{title}</S.Title>
+          <S.Developer>{developer}</S.Developer>
+        </S.Info>
+
+        <S.FavButton role="button">
+          <FavoriteBorder aria-label="Add to wishlist" />
+        </S.FavButton>
+
+        <S.BuyBox>
+          {!!promotionalPrice && <S.Price isPromotional>{price}</S.Price>}
+          <S.Price>{promotionalPrice || price}</S.Price>
+          <Button icon={<AddShoppingCart />} size="small" />
+        </S.BuyBox>
+      </S.Content>
+    </S.Wrapper>
+  )
+}
+
+export default GameCard
---
+++ src/components/GameCard/stories.tsx
@@ -0,0 +1,20 @@
+import GameCard, { GameCardProps } from '.'
+import { Meta, StoryFn } from '@storybook/react'
+
+export default {
+  title: 'GameCard',
+  component: GameCard,
+  args: {
+    title: 'Population Zero',
+    developer: 'Rockstar Studios',
+    img: '/img/gamecard-img.png',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 200,00'
+  }
+} as Meta
+
+export const Basic: StoryFn<GameCardProps> = (args) => (
+  <div style={{ width: '30rem' }}>
+    <GameCard {...args} />
+  </div>
+)
