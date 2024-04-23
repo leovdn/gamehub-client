6362f6c501db110f8a5fa19fe5a60937e7ce7a90
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-04-23 13:06:47 -0300

    test: ribbon for gamecards

--- src/components/GameCard/test.tsx
+++ src/components/GameCard/test.tsx
@@ -86,4 +86,28 @@ describe('<GameCard />', () => {
 
     expect(onFavorite).toHaveBeenCalled()
   })
+
+  it('should render a Ribbon', () => {
+    renderWithTheme(
+      <GameCard
+        {...props}
+        ribbon="My Ribbon"
+        ribbonColor="secondary"
+        ribbonSize="small"
+      />
+    )
+
+    const ribbon = screen.getByText(/My Ribbon/i)
+
+    expect(ribbon).toHaveStyle({
+      backgroundColor: '#3CD3C1'
+    })
+
+    expect(ribbon).toHaveStyle({
+      height: '2.6rem',
+      fontSize: '1.2rem'
+    })
+
+    expect(ribbon).toBeInTheDocument()
+  })
 })
