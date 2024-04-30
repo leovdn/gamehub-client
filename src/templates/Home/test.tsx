42362672e0b9c59e92dbbc751211d521d50452ce
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-04-30 18:41:16 -0300

    test: home page sections

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -2,24 +2,52 @@ import { screen } from '@testing-library/react'
 
 import Home from './index'
 import { renderWithTheme } from 'utils/tests/helpers'
+import bannerSlideMockItems from 'components/BannerSlider/mock'
+import gameCardSliderMockItems from 'components/GameCardSlider/mock'
+import highlightMockItem from 'components/Highlight/mock'
+
+const props = {
+  banners: bannerSlideMockItems,
+  newGames: [gameCardSliderMockItems[0]],
+  mostPopularHighlight: highlightMockItem,
+  mostPopularGames: [gameCardSliderMockItems[0]],
+  upcommingGames: [gameCardSliderMockItems[0]],
+  upcommingHighligth: highlightMockItem,
+  upcommingMoreGames: [gameCardSliderMockItems[0]],
+  freeGames: [gameCardSliderMockItems[0]],
+  freeHighligth: highlightMockItem
+}
 
 describe('<Home />', () => {
   it('should render menu and footer', () => {
-    renderWithTheme(<Home />)
+    renderWithTheme(<Home {...props} />)
 
+    // menu
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
 
+    // footer
     expect(
-      screen.getByRole('heading', { name: /contact/i })
+      screen.getByRole('heading', { name: /follow us/i })
     ).toBeInTheDocument()
-  })
 
-  it('should render the sections', () => {
-    renderWithTheme(<Home />)
+    // logos (menu/footer)
+    expect(screen.getAllByRole('img', { name: /gamehub/i })).toHaveLength(2)
 
-    expect(screen.getByText(/News/i)).toBeInTheDocument()
-    expect(screen.getByText(/Most Popular/i)).toBeInTheDocument()
-    expect(screen.getByText(/Upcoming/i)).toBeInTheDocument()
-    expect(screen.getByText(/Free Games/i)).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /most popular/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /upcoming/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /free games/i })
+    ).toBeInTheDocument()
+
+    // // banner
+    // expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1)
+    // // card game ( 5 sections com 1 card cada = 5x1 = 5)
+    // expect(screen.getAllByText(/population zero/i)).toHaveLength(5)
+    // // highlight
+    // expect(screen.getAllByText(/read dead is back!/i)).toHaveLength(3)
   })
 })
