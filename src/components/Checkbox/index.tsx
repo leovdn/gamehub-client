c3dfbcfb8ef2158df8f36b352753a7e813f4cbb0
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-05-03 16:20:48 -0300

    feat: add checkbox label colors

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -3,13 +3,22 @@ import * as S from './styles'
 export type CheckboxProps = {
   label?: string
   labelFor?: string
+  labelColor?: 'black' | 'white'
 }
 
-const Checkbox = ({ label, labelFor = '' }: CheckboxProps) => {
+const Checkbox = ({
+  label,
+  labelFor = '',
+  labelColor = 'white'
+}: CheckboxProps) => {
   return (
     <S.Wrapper>
       <S.Checkbox type="checkbox" id={labelFor} />
-      {label && <S.Label htmlFor={labelFor}>{label}</S.Label>}
+      {label && (
+        <S.Label htmlFor={labelFor} labelColor={labelColor}>
+          {label}
+        </S.Label>
+      )}
     </S.Wrapper>
   )
 }
--- src/components/Checkbox/styles.ts
+++ src/components/Checkbox/styles.ts
@@ -1,7 +1,16 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { CheckboxProps } from '.'
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+`
 
 export const Checkbox = styled.input``
 
-export const Label = styled.label``
+export const Label = styled.label<Pick<CheckboxProps, 'labelColor'>>`
+  ${({ theme, labelColor }) => css`
+    color: ${theme.colors[labelColor!]};
+    margin-left: ${theme.spacings.xxsmall};
+  `}
+`
