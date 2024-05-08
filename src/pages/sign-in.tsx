ca973acfa9010f527fb94f658929058656d8ad58
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-05-08 17:15:42 -0300

    feat: create sign-in and sign-up pages

---
+++ src/pages/sign-in.tsx
@@ -0,0 +1,9 @@
+import Auth from 'templates/Auth'
+
+export default function SignIn() {
+  return (
+    <Auth title="SignIn">
+      <p>Sign in Form</p>
+    </Auth>
+  )
+}
---
+++ src/pages/sign-up.tsx
@@ -0,0 +1,9 @@
+import Auth from 'templates/Auth'
+
+export default function SignUp() {
+  return (
+    <Auth title="SignUp">
+      <p>Sign Up Form</p>
+    </Auth>
+  )
+}
