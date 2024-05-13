f52914834fb30a2e256d729cad2c7a16c8257014
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-05-13 16:29:28 -0300

    test: signUp form components

---
+++ src/components/FormSignUp/test.tsx
@@ -0,0 +1,27 @@
+import { screen } from '@testing-library/react'
+
+import FormSignUp from './index'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+describe('<FormSignUp />', () => {
+  it('should render the form inputs', () => {
+    renderWithTheme(<FormSignUp />)
+
+    expect(screen.getByPlaceholderText(/Full Name/)).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/Email/)).toBeInTheDocument()
+    expect(screen.getByPlaceholderText('Password')).toBeInTheDocument()
+    expect(screen.getByPlaceholderText('Confirm Password')).toBeInTheDocument()
+    expect(
+      screen.getByRole('button', { name: /Create account/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the sign in link', () => {
+    renderWithTheme(<FormSignUp />)
+
+    const signInLink = screen.getByRole('link', { name: /Sign in/ })
+
+    expect(signInLink).toBeInTheDocument()
+    expect(signInLink).toHaveAttribute('href', '/sign-in')
+  })
+})
