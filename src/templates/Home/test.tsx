2c5d3c53b40697b343631089da159c3d478079e6
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-05-14 17:23:13 -0300

    refactor: Home tests execution time

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -18,46 +18,56 @@ const props = {
   freeHighlight: highlightMock
 }
 
+jest.mock('components/Menu', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="Menu Mock" />
+    }
+  }
+})
+
+jest.mock('components/Footer', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="Footer Mock" />
+    }
+  }
+})
+
+jest.mock('components/Showcase', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="Showcase Mock" />
+    }
+  }
+})
+
+jest.mock('components/BannerSlider', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="BannerSlider Mock" />
+    }
+  }
+})
+
 describe('<Home />', () => {
   it('should render menu and footer', () => {
     renderWithTheme(<Home {...props} />)
 
     // menu
-    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
-
+    expect(screen.getByTestId('Menu Mock')).toBeInTheDocument()
     // footer
-    expect(
-      screen.getByRole('heading', { name: /follow us/i })
-    ).toBeInTheDocument()
-
-    // logos (menu/footer)
-    expect(screen.getAllByRole('img', { name: /gamehub/i })).toHaveLength(2)
+    expect(screen.getByTestId('Footer Mock')).toBeInTheDocument()
   })
 
   it('should render the Home page contents', () => {
     renderWithTheme(<Home {...props} />)
 
-    expect(screen.getByRole('heading', { name: /New/ })).toBeInTheDocument()
-
-    expect(
-      screen.getByRole('heading', { name: /most popular/i })
-    ).toBeInTheDocument()
-
-    expect(
-      screen.getByRole('heading', { name: /upcoming/i })
-    ).toBeInTheDocument()
-
-    expect(
-      screen.getByRole('heading', { name: /free games/i })
-    ).toBeInTheDocument()
-
-    // banner
-    expect(screen.getAllByText(/Board Game/i)).toHaveLength(2)
-    // Gamecard (5 sliders with only 1 element each)
-    expect(screen.getAllByText(/population zero/i)).toHaveLength(5)
-    // highlight
-    expect(screen.getAllByRole('heading', { name: /Red Dead/i })).toHaveLength(
-      3
-    )
+    expect(screen.getByTestId('BannerSlider Mock')).toBeInTheDocument()
+    expect(screen.getAllByTestId('Showcase Mock')).toHaveLength(5)
   })
 })
