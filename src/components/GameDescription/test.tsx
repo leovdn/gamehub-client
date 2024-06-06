4d088894e49cfee38e99a35c3ee5754d6822bfc0
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-06-06 14:27:06 -0300

    test: GameDescription content for innerHTML

--- src/components/GameDescription/test.tsx
+++ src/components/GameDescription/test.tsx
@@ -4,18 +4,41 @@ import GameDescription from './index'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 const props = {
-  image: '/mock.png',
-  full_description: 'Full description',
-  short_description: 'short'
+  title: 'Description',
+  content: 'Description content'
 }
 
 describe('<GameDescription />', () => {
-  it('should render the heading', () => {
+  it('should render the title and content', () => {
     renderWithTheme(<GameDescription {...props} />)
 
-    expect(screen.getByText(/short/i, { selector: 'p' })).toBeInTheDocument()
     expect(
-      screen.getByText(/Full description/i, { selector: 'p' })
+      screen.getByRole('heading', { name: /Description/i })
     ).toBeInTheDocument()
+    expect(screen.getByText(/Description content/i)).toBeInTheDocument()
+  })
+
+  it('should render without title', () => {
+    renderWithTheme(<GameDescription content={props.content} />)
+
+    expect(
+      screen.queryByRole('heading', { name: /Description/i })
+    ).not.toBeInTheDocument()
+  })
+
+  it('should render the title and content', () => {
+    renderWithTheme(<GameDescription {...props} />)
+
+    const wrapper = screen.getByRole('heading', {
+      name: /Description/i
+    }).parentElement
+
+    expect(wrapper).toHaveStyle({
+      color: '#FAFAFA'
+    })
+
+    expect(wrapper).toHaveStyleRule('color', '#030517', {
+      media: '(min-width: 768px)'
+    })
   })
 })
