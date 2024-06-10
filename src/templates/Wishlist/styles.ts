332ce7a41e5b4e4e5ffa3d0840d082e9c0e9a693
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-06-10 15:40:40 -0300

    style: responsive wishlist cards

--- src/templates/Wishlist/styles.ts
+++ src/templates/Wishlist/styles.ts
@@ -4,19 +4,7 @@ export const WishlistContent = styled.section`
   ${({ theme }) => css`
     margin-top: ${theme.spacings.small};
     display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    gap: 2rem;
-
-    @media (max-width: ${theme.breakpoints.large}) {
-      grid-template-columns: repeat(3, 1fr);
-    }
-
-    @media (max-width: ${theme.breakpoints.medium}) {
-      grid-template-columns: repeat(2, 1fr);
-    }
-
-    @media (max-width: ${theme.breakpoints.small}) {
-      grid-template-columns: repeat(1, 1fr);
-    }
+    grid-template-columns: repeat(auto-fill, minmax(25rem, 1fr));
+    grid-gap: 3rem;
   `}
 `
