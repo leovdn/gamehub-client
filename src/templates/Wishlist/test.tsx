ea0820f9ea4e93203a370d7b420e5d26f7fda20d
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-06-10 15:33:26 -0300

    test: wishlist page components

---
+++ src/templates/Wishlist/test.tsx
@@ -0,0 +1,43 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Wishlist, { WishlistTemplateProps } from './index'
+import gameCardSliderMockItems from 'components/GameCardSlider/mock'
+import highlightMockItem from 'components/Highlight/mock'
+
+const props: WishlistTemplateProps = {
+  games: gameCardSliderMockItems,
+  recommendedGames: gameCardSliderMockItems,
+  recommendedHighlight: highlightMockItem
+}
+
+jest.mock('components/Showcase', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="Showcase Mock" />
+    }
+  }
+})
+
+jest.mock('components/GameCard', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="GameCard Mock" />
+    }
+  }
+})
+
+describe('<Wishlist />', () => {
+  it('should render the heading', () => {
+    renderWithTheme(<Wishlist {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /Wishlist/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getAllByTestId('GameCard Mock')).toHaveLength(5)
+    expect(screen.getAllByTestId('Showcase Mock')).toHaveLength(1)
+  })
+})
