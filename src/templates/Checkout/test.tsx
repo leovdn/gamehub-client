bfbc73da36e2c5c564090f9566102ac94c6ca0ed
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-06-11 18:31:09 -0300

    test: cart page

---
+++ src/templates/Checkout/test.tsx
@@ -0,0 +1,47 @@
+import { screen } from '@testing-library/react'
+
+import Checkout, { CheckoutTemplateProps } from './index'
+import { mockGameItems } from 'components/GameItem/mock'
+import { mockCreditCards } from 'components/PaymentOptions/mock'
+import gameCardSliderMockItems from 'components/GameCardSlider/mock'
+import highlightMockItem from 'components/Highlight/mock'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+const props: CheckoutTemplateProps = {
+  cart: {
+    total: 'R$ 430,00',
+    items: mockGameItems,
+    modal: false
+  },
+  cards: mockCreditCards,
+  recommendedGames: gameCardSliderMockItems,
+  recommendedHighlight: highlightMockItem
+}
+
+jest.mock('components/Showcase', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="Showcase Mock" />
+    }
+  }
+})
+
+jest.mock('components/PaymentOptions', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="PaymentOptions Mock" />
+    }
+  }
+})
+
+describe('<Checkout />', () => {
+  it('should render the cart section', () => {
+    renderWithTheme(<Checkout {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /My Cart/i })
+    ).toBeInTheDocument()
+  })
+})
