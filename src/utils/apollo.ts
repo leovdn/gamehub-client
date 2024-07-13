9535e43da4bfbce1acfefe0020bc70e43a97433c
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-07-13 20:53:07 -0300

    fix: apollo credentials

--- src/utils/apollo.ts
+++ src/utils/apollo.ts
@@ -6,7 +6,10 @@ let apolloClient: ApolloClient<NormalizedCacheObject>
 function createApolloClient() {
   return new ApolloClient({
     ssrMode: typeof window === 'undefined',
-    link: new HttpLink({ uri: process.env.NEXT_PUBLIC_STRAPI_GRAPHQL_API }),
+    link: new HttpLink({
+      uri: 'http://localhost:1337/graphql',
+      credentials: 'same-origin'
+    }),
     cache: new InMemoryCache()
   })
 }
