04f476303482509cc516ad2a5edf6173cd8407ae
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-08-29 17:40:34 -0300

    feat: add home query with dynamic cards, sections and highlights

--- src/graphql/queries/home.tsx
+++ src/graphql/queries/home.tsx
@@ -10,13 +10,86 @@ export const QUERY_HOME = gql(`
         }
       }
     }
-    freeGames  {
+    newGames: games(
+      filters: { release_date: { gt: "2024-08-01" } }
+      sort: "release_date:desc"
+      pagination: { limit: 8 }
+    ) {
       data {
         id
         attributes {
-          ...FreeGamesFragment
+          ...GameFragment
         }
       }
     }
+    upcommingGames: games(
+      filters: { release_date: { gt: "2024-08-26" } }
+      sort: "release_date:asc"
+      pagination: { limit: 8 }
+    ) {
+      data {
+        id
+        attributes {
+          ...GameFragment
+        }
+      }
+    }
+    freeGames: games(
+      filters: { price: { eq: 0 } }
+      sort: "release_date:desc"
+      pagination: { limit: 8 }
+    ) {
+      data {
+        id
+        attributes {
+          ...GameFragment
+        }
+      }
+    }
+    sections: home {
+      data {
+        attributes {
+          newGames {
+            id
+            title
+            highlight {
+              ...HighlightFragment
+            }
+          }
+
+          upcommingGames {
+            id
+            title
+            highlight {
+              ...HighlightFragment
+            }
+          }
+
+          freeGames {
+            id
+            title
+            highlight {
+              ...HighlightFragment
+            }
+          }
+
+          popularGames {
+            id
+            title
+            highlight {
+              ...HighlightFragment
+            }
+            games {
+              data {
+                attributes {
+                  ...GameFragment
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
   }
 `)
